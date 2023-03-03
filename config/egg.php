3715a6bd52a6c17e349ac92bbd5af83a690f477c
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-03-03 12:15:47 +0100

    first commit

---
+++ config/egg.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    [
+        "color" => "blu",
+        "contenuto" => "gioco",
+        "tipo_cioccolato" => "latte",
+        "grandezza" => "2 metri"
+
+    ],
+    [
+        "color" => "blu",
+        "contenuto" => "gioco",
+        "tipo_cioccolato" => "latte",
+        "grandezza" => "2 metri"
+    ]
+];
